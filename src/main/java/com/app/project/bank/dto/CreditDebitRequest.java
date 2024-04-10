df43163024b25b85012f686419e19df7d156b396
Isaque Scheidegger <isaquescheidegger@gmail.com>
2024-04-10 12:51:21 -0300

    Credit and Debit WORKING

---
+++ src/main/java/com/app/project/bank/dto/CreditDebitRequest.java
@@ -0,0 +1,15 @@
+package com.app.project.bank.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreditDebitRequest {
+    private String accountNumber;
+    private BigDecimal amount;
+}
---
+++ src/main/java/com/app/project/bank/service/impl/EmailService.java
@@ -0,0 +1,8 @@
+package com.app.project.bank.service.impl;
+
+import com.app.project.bank.dto.EmailDetails;
+
+public interface EmailService {
+    void sendEmailAlert (EmailDetails emailDetails);
+
+}
